914546979996a443d64a674aefd8a21344c4e571
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-01 16:26:21 -0300

    feat: realized get Product and Order

--- src/main/java/br/com/cleilsonandrade/dscommerce/entities/Order.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/entities/Order.java
@@ -2,6 +2,7 @@ package br.com.cleilsonandrade.dscommerce.entities;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -85,6 +86,14 @@ public class Order {
 
   public void setPayment(Payment payment) {
     this.payment = payment;
+  }
+
+  public Set<OrderItem> getItems() {
+    return items;
+  }
+
+  public List<Product> getProducts() {
+    return items.stream().map(x -> x.getProduct()).toList();
   }
 
 }
--- src/main/java/br/com/cleilsonandrade/dscommerce/entities/Product.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package br.com.cleilsonandrade.dscommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -87,6 +88,14 @@ public class Product {
 
   public Set<Category> getCategories() {
     return categories;
+  }
+
+  public Set<OrderItem> getItems() {
+    return items;
+  }
+
+  public List<Order> getOrders() {
+    return items.stream().map(x -> x.getOrder()).toList();
   }
 
 }
