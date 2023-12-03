724f4941f96c98ea323c5a8cf53ee50ab25640f5
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-03 01:34:59 -0300

    feat: get product by id

--- src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
@@ -1,15 +1,22 @@
 package br.com.cleilsonandrade.dscommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.cleilsonandrade.dscommerce.repositories.ProductRepository;
+import br.com.cleilsonandrade.dscommerce.dto.ProductDTO;
+import br.com.cleilsonandrade.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
   @Autowired
-  private ProductRepository productRepository;
+  private ProductService productService;
 
+  @GetMapping("/{id}")
+  public ProductDTO findById(@PathVariable Long id) {
+    return productService.findById(id);
+  }
 }
