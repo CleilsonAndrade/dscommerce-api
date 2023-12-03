6e3ef90b6f16ba1e00dc0ec4566cf73bd2b7e7eb
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-02 23:36:59 -0300

    feat: add equals and hashCode in entities

--- src/main/java/br/com/cleilsonandrade/dscommerce/entities/Order.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/entities/Order.java
@@ -96,4 +96,29 @@ public class Order {
     return items.stream().map(x -> x.getProduct()).toList();
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Order other = (Order) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
 }
--- src/main/java/br/com/cleilsonandrade/dscommerce/entities/OrderItemPK.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/entities/OrderItemPK.java
@@ -33,4 +33,35 @@ public class OrderItemPK {
     this.product = product;
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((order == null) ? 0 : order.hashCode());
+    result = prime * result + ((product == null) ? 0 : product.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    OrderItemPK other = (OrderItemPK) obj;
+    if (order == null) {
+      if (other.order != null)
+        return false;
+    } else if (!order.equals(other.order))
+      return false;
+    if (product == null) {
+      if (other.product != null)
+        return false;
+    } else if (!product.equals(other.product))
+      return false;
+    return true;
+  }
+
 }
