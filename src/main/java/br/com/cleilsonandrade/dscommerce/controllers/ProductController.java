a2e1106d66997439032b679ac61cc5a1c6068dde
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-04 15:58:32 -0300

    feat: get all products paged

--- src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package br.com.cleilsonandrade.dscommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +20,10 @@ public class ProductController {
   @GetMapping("/{id}")
   public ProductDTO findById(@PathVariable Long id) {
     return productService.findById(id);
+  }
+
+  @GetMapping
+  public Page<ProductDTO> findAll(Pageable pageable) {
+    return productService.findAll(pageable);
   }
 }
--- src/main/java/br/com/cleilsonandrade/dscommerce/services/ProductService.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/services/ProductService.java
@@ -1,6 +1,8 @@
 package br.com.cleilsonandrade.dscommerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +19,11 @@ public class ProductService {
   public ProductDTO findById(Long id) {
     Product result = repository.findById(id).get();
     return new ProductDTO(result);
+  }
+
+  @Transactional(readOnly = true)
+  public Page<ProductDTO> findAll(Pageable pageable) {
+    Page<Product> result = repository.findAll(pageable);
+    return result.map(x -> new ProductDTO(x));
   }
 }
