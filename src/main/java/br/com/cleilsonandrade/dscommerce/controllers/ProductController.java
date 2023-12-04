d32aa0b0c3011408cf10110abeaf989f55a79296
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-04 16:15:08 -0300

    feat: implement create product resource

--- src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,10 @@ public class ProductController {
   @GetMapping
   public Page<ProductDTO> findAll(Pageable pageable) {
     return productService.findAll(pageable);
+  }
+
+  @PostMapping
+  public ProductDTO insert(@RequestBody ProductDTO dto) {
+    return productService.insert(dto);
   }
 }
--- src/main/java/br/com/cleilsonandrade/dscommerce/services/ProductService.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/services/ProductService.java
@@ -26,4 +26,17 @@ public class ProductService {
     Page<Product> result = repository.findAll(pageable);
     return result.map(x -> new ProductDTO(x));
   }
+
+  @Transactional
+  public ProductDTO insert(ProductDTO dto) {
+    Product entity = new Product();
+    entity.setName(dto.getName());
+    entity.setDescription(dto.getDescription());
+    entity.setPrice(dto.getPrice());
+    entity.setImgUrl(dto.getImgUrl());
+
+    entity = repository.save(entity);
+
+    return new ProductDTO(entity);
+  }
 }
