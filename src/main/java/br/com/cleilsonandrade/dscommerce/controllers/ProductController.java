1d0c051b025e38b1543637b93d0879bd321b2c67
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-05 18:50:14 -0300

    feat: update product and refactor service

--- src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +42,11 @@ public class ProductController {
     URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 
     return ResponseEntity.created(uri).body(dto);
+  }
+
+  @PutMapping
+  public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+    dto = productService.update(id, dto);
+    return ResponseEntity.ok(dto);
   }
 }
--- src/main/java/br/com/cleilsonandrade/dscommerce/services/ProductService.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/services/ProductService.java
@@ -30,13 +30,29 @@ public class ProductService {
   @Transactional
   public ProductDTO insert(ProductDTO dto) {
     Product entity = new Product();
-    entity.setName(dto.getName());
-    entity.setDescription(dto.getDescription());
-    entity.setPrice(dto.getPrice());
-    entity.setImgUrl(dto.getImgUrl());
+
+    copyDtoToEntity(dto, entity);
 
     entity = repository.save(entity);
 
     return new ProductDTO(entity);
+  }
+
+  @Transactional
+  public ProductDTO update(Long id, ProductDTO dto) {
+    Product entity = repository.getReferenceById(id);
+
+    copyDtoToEntity(dto, entity);
+
+    entity = repository.save(entity);
+
+    return new ProductDTO(entity);
+  }
+
+  private void copyDtoToEntity(ProductDTO dto, Product entity) {
+    entity.setName(dto.getName());
+    entity.setDescription(dto.getDescription());
+    entity.setPrice(dto.getPrice());
+    entity.setImgUrl(dto.getImgUrl());
   }
 }
