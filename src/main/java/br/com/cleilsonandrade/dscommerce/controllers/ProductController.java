137aed6df37ac91fbef97ea17fc1a321e00d38a8
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-05 19:04:19 -0300

    ✨ feat: delete product resource

--- src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,11 @@ public class ProductController {
   public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
     dto = productService.update(id, dto);
     return ResponseEntity.ok(dto);
+  }
+
+  @DeleteMapping
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    productService.delete(id);
+    return ResponseEntity.noContent().build();
   }
 }
--- src/main/java/br/com/cleilsonandrade/dscommerce/services/ProductService.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/services/ProductService.java
@@ -49,6 +49,11 @@ public class ProductService {
     return new ProductDTO(entity);
   }
 
+  @Transactional
+  public void delete(Long id) {
+    repository.deleteById(id);
+  }
+
   private void copyDtoToEntity(ProductDTO dto, Product entity) {
     entity.setName(dto.getName());
     entity.setDescription(dto.getDescription());
