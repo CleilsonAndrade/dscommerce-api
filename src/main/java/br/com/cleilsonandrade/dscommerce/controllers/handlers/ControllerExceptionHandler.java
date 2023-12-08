714f726d66a797dec7d1e566003274b43c48b1ba
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-07 22:29:05 -0300

    🔎 ref: custom message exception body request

--- src/main/java/br/com/cleilsonandrade/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -4,10 +4,13 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import br.com.cleilsonandrade.dscommerce.dto.CustomError;
+import br.com.cleilsonandrade.dscommerce.dto.CustomErrorDTO;
+import br.com.cleilsonandrade.dscommerce.dto.ValidationErrorDTO;
 import br.com.cleilsonandrade.dscommerce.services.exceptions.DatabaseException;
 import br.com.cleilsonandrade.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,18 +18,32 @@ import jakarta.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ControllerExceptionHandler {
   @ExceptionHandler(ResourceNotFoundException.class)
-  public ResponseEntity<CustomError> resourceNotFoundException(ResourceNotFoundException e,
+  public ResponseEntity<CustomErrorDTO> resourceNotFoundException(ResourceNotFoundException e,
       HttpServletRequest request) {
     HttpStatus status = HttpStatus.NOT_FOUND;
-    CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+    CustomErrorDTO err = new CustomErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
     return ResponseEntity.status(status).body(err);
   }
 
   @ExceptionHandler(DatabaseException.class)
-  public ResponseEntity<CustomError> databaseException(DatabaseException e,
+  public ResponseEntity<CustomErrorDTO> database(DatabaseException e,
       HttpServletRequest request) {
     HttpStatus status = HttpStatus.BAD_REQUEST;
-    CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+    CustomErrorDTO err = new CustomErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+    return ResponseEntity.status(status).body(err);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<CustomErrorDTO> methodArgumentNotValid(MethodArgumentNotValidException e,
+      HttpServletRequest request) {
+    HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+    ValidationErrorDTO err = new ValidationErrorDTO(Instant.now(), status.value(), "Invalid data",
+        request.getRequestURI());
+
+    for (FieldError f : e.getBindingResult().getFieldErrors()) {
+      err.addError(f.getField(), f.getDefaultMessage());
+    }
+
     return ResponseEntity.status(status).body(err);
   }
 }
--- src/main/java/br/com/cleilsonandrade/dscommerce/dto/CustomErrorDTO.java
+++ src/main/java/br/com/cleilsonandrade/dscommerce/dto/CustomErrorDTO.java
@@ -2,13 +2,13 @@ package br.com.cleilsonandrade.dscommerce.dto;
 
 import java.time.Instant;
 
-public class CustomError {
+public class CustomErrorDTO {
   private Instant timestamp;
   private Integer status;
   private String error;
   private String path;
 
-  public CustomError(Instant timestamp, Integer status, String error, String path) {
+  public CustomErrorDTO(Instant timestamp, Integer status, String error, String path) {
     this.timestamp = timestamp;
     this.status = status;
     this.error = error;
